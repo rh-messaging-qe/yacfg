019432fad1bbb7c130cb183e3a3799e4f83de33f
Dominik Lenoch <dominiklenoch@gmail.com>
2020-10-06 12:39:55 +0200

    VERSION 0.8.0

--- src/yacfg/meta.py
+++ src/yacfg/meta.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 NAME = 'yacfg'
-VERSION = '0.7.0'
+VERSION = '0.8.0'
 SHORT_DESCRIPTION = 'Template based configuration generator'
 DESCRIPTION = (
     'Template based configuration files generator based on jinja2 and yaml'
