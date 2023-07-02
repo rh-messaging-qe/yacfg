cdeb8daf609baee09cc2d9bfc19e3abaca57e1f4
Dominik Lenosi <lenosi@proton.me>
2023-07-02 21:13:13 +0200

    refactor(yacfg): Refactor logger_settings.py
    - Refactoring the config_console_logger() function to improve code structure and readability.
    - Adding type annotations to function parameters and return types.
    - Setting the default logger level to DEBUG.
    - Implementing a file logging feature using a rotating file handler, allowing log files to rotate based on size and keeping a specified number of backup log files.
    
    These changes aim to improve the functionality and maintainability of the logger_settings.py module.

--- src/yacfg/logger_settings.py
+++ src/yacfg/logger_settings.py
@@ -1,75 +1,44 @@
-# Copyright 2018 Red Hat Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import logging
-import sys
-
-FORMAT_CONSOLE = "[%(asctime)s] [%(levelname)s] %(name)s :: %(message)s"
-FORMAT_CONSOLE_COLOR = (
-    "[%(asctime)s] [%(log_color)s%(levelname)s%(reset)s]" " %(name)s :: %(message)s"
-)
-
-DATE_FORMAT_ISO = "%Y-%m-%d %H:%M:%S,"
-DATE_FORMAT_TIME = "%H:%M:%S"
+from logging.handlers import RotatingFileHandler
+from typing import Optional
 
 
 def config_console_logger(
-    filename=None, level=None, fmt=None, fmt_color=None, datefmt=None
-):
-    """configure console logger, if colorlog is installed it will use colorlog
-    :param filename: stream to log into, default sys.stdout
-    :param level: console log level
-    :param fmt: console log format
-    :param fmt_color: console color log format (only with colorlog)
-    :param datefmt: formatter date format, default like ISO8601
+    filename: Optional[str] = None,
+    fmt: Optional[str] = None,
+    fmt_color: Optional[str] = None,
+    datefmt: Optional[str] = None,
+    level=logging.DEBUG,
+    max_bytes: int = 50000,
+    backup_count: int = 3,
+) -> None:
     """
-    import_error_msg = []
-    # Windows compatibility section for colorama
+    Configure a console and file logger with optional color support and rotating file handler.
+    If colorlog is installed, it will use colorlog for colored output.
+
+    :param filename: Stream to log into, default is sys.stdout.
+    :param level: Console log level.
+    :param fmt: Console log format.
+    :param fmt_color: Console color log format (only with colorlog).
+    :param datefmt: Formatter date format, default is ISO8601.
+    :param max_bytes: Maximum log file size before rotation, default is 50000.
+    :param backup_count: Number of backup log files to keep, default is 3.
+    """
+
+    level = level or logging.DEBUG
+    fmt = fmt or "[%(asctime)s] [%(levelname)s] %(name)s :: %(message)s"
+    fmt_color = fmt_color or (
+        "[%(asctime)s] [%(log_color)s%(levelname)s%(reset)s] %(name)s :: %(message)s"
+    )
+    datefmt = datefmt or "%H:%M:%S"
+
     try:
-        # noinspection PyUnresolvedReferences
-        import colorama
+        import colorlog  # type: ignore
+        import colorama  # type: ignore
 
         colorama.init()
-    except ImportError:
-        try:
-            # noinspection PyUnresolvedReferences
-            import colorlog
-        except ImportError:
-            import platform
 
-            if platform.system() == "Windows":
-                import_error_msg.append(
-                    "Colorlog without colorama used, expect escape codes,"
-                    " please install colorama"
-                )
-
-    filename = filename or sys.stdout
-    level = level or logging.DEBUG
-    fmt = fmt or FORMAT_CONSOLE
-    fmt_color = fmt_color or FORMAT_CONSOLE_COLOR
-    datefmt = datefmt or DATE_FORMAT_TIME
-
-    # console handler
-    # console_handler = logging.StreamHandler(stream=filename)  # Python 2.7
-    console_handler = logging.StreamHandler()  # Python 2.6
-    console_handler.setLevel(level)
-    console_formatter = logging.Formatter(fmt, datefmt=datefmt)
-
-    try:
-        # noinspection PyUnresolvedReferences
-        import colorlog  # noqa: F811
-
+        # Create the console_formatter with the extended log_colors
         console_formatter = colorlog.ColoredFormatter(
             fmt_color,
             log_colors={
@@ -83,20 +52,29 @@ def config_console_logger(
                 "DOC": "purple",
                 "DEBUG": "cyan",
             },
+            reset=True,
             datefmt=datefmt,
         )
+
     except ImportError:
-        # import_error_msg.append(
-        #     "cannot initialize colorlog, using boring streamhandler"
-        # )
-        pass
+        import sys
+
+        console_formatter = logging.Formatter(fmt, datefmt=datefmt)
+
+    console_handler: logging.Handler = logging.StreamHandler()
     console_handler.setFormatter(console_formatter)
+
+    # Set default logger
+    logging.getLogger().setLevel(level)
+
+    # Add the console_handler to the logger
     logging.getLogger().addHandler(console_handler)
-    # log all above error messages
-    if import_error_msg:
-        for log_err in import_error_msg:
-            logging.info(log_err)
 
-
-# default logger level INFO
-logging.getLogger().setLevel(logging.INFO)
+    # File logging
+    if filename:
+        file_handler = RotatingFileHandler(
+            filename, maxBytes=max_bytes, backupCount=backup_count
+        )
+        file_formatter = logging.Formatter(fmt, datefmt=datefmt)
+        file_handler.setFormatter(file_formatter)
+        logging.getLogger().addHandler(file_handler)
