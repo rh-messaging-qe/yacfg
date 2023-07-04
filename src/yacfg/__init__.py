1feac33c4a517fc2c57c017063af3036af173d5d
Dominik Lenosi <lenosi@proton.me>
2023-07-04 13:39:53 +0200

    refactor(yacfg): Refactor package metadata and descriptions
    Refactor the metadata and descriptions for the 'yacfg' and 'yacfg-batch' packages.

--- src/yacfg/__init__.py
+++ src/yacfg/__init__.py
@@ -1,17 +1,3 @@
-# Copyright 2018 Red Hat Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 try:
     from importlib import metadata
 except ImportError:
@@ -22,4 +8,9 @@ try:
 except metadata.PackageNotFoundError:
     __version__ = "devel"
 
-from .yacfg import generate  # noqa: F401
+NAME = "yacfg"
+SHORT_DESCRIPTION = "Template based configuration generator"
+DESCRIPTION = (
+    "Template based configuration files generator based on jinja2 and yaml"
+    " mainly focused on Apache ActiveMQ Artemis and related projects"
+)
