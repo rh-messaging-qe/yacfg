208ee5a96202adb94b2e78f401c54a4725893345
Dominik Lenosi <lenosi@proton.me>
2023-07-04 13:39:53 +0200

    refactor(yacfg): Refactor package metadata and descriptions
    Refactor the metadata and descriptions for the 'yacfg' and 'yacfg-batch' packages.

--- src/yacfg_batch/__init__.py
+++ src/yacfg_batch/__init__.py
@@ -1,3 +1,12 @@
 from yacfg import __version__ as yacfg_version
 
 __version__ = yacfg_version
+
+NAME = "yacfg-batch"
+SHORT_DESCRIPTION = "Template based configuration generator"
+DESCRIPTION = (
+    "Batch configuration generator supporting generation of multiple services"
+    " from a set of profiles using the template based configuration files"
+    " generator 'yacfg' based on jinja2 and yaml mainly focused on Apache"
+    " ActiveMQ Artemis and related projects"
+)
