fc2afe7b40cd95fe68e47da0b8d82c3f37e52fa9
Dominik Lenosi <lenosi@proton.me>
2023-07-04 20:25:50 +0200

    refactor(templates): Improve get_template_environment function
    - Updated function signature to include type hint for template_name parameter and specify return type as Environment.
    - Revised docstring to provide detailed information about function purpose, parameters, return type, and potential exceptions.
    - Refactored code to use ChoiceLoader and FileSystemLoader for template loading.

--- src/yacfg/templates.py
+++ src/yacfg/templates.py
@@ -9,15 +9,14 @@ from .files import get_templates_paths, select_template_dir
 LOG: logging.Logger = logging.getLogger(NAME)
 
 
-def get_template_environment(template_name):
-    """Create Jinja2 environment for the selected template.
+def get_template_environment(template_name: str) -> Environment:
+    """
+    Create Jinja2 environment for the selected template.
 
     :param template_name: Name of the template set or path to a user-specified template set.
     :type template_name: str
-
     :return: Jinja2 environment.
     :rtype: Environment
-
     :raises TemplateError: If there is a problem with the templating environment.
     """
     LOG.debug(f"Template name: {template_name}")
@@ -25,20 +24,17 @@ def get_template_environment(template_name):
     selected_template_path = select_template_dir(template_name)
     LOG.debug(f"Selected template path: {selected_template_path}")
 
-    loader = ChoiceLoader(
-        [
-            FileSystemLoader(selected_template_path),
-            FileSystemLoader(get_templates_paths()),
-        ]
-    )
-    extensions = ["jinja2_ansible_filters.AnsibleCoreFiltersExtension"]
-
     try:
+        loader = ChoiceLoader(
+            [
+                FileSystemLoader(selected_template_path),
+                FileSystemLoader(get_templates_paths()),
+            ]
+        )
+        extensions = ["jinja2_ansible_filters.AnsibleCoreFiltersExtension"]
+
         env = Environment(
-            loader=loader,
-            trim_blocks=True,
-            lstrip_blocks=True,
-            extensions=extensions
+            loader=loader, trim_blocks=True, lstrip_blocks=True, extensions=extensions
         )
     except Exception as e:
         LOG.exception("Error creating the Jinja2 environment.")
